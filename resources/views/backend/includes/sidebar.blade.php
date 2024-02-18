66c60eebdd14f286c7c86e17fc84009dc2a39737
mrsharifull <116838065+mrsharifull@users.noreply.github.com>
2024-02-18 12:54:38 +0600

    Update

---
+++ resources/views/backend/includes/sidebar.blade.php
@@ -0,0 +1,16 @@
+<div class="sidebar">
+    <div class="sidebar-wrapper">
+        <div class="logo">
+            <a href="#" class="simple-text logo-mini">{{ _('WD') }}</a>
+            <a href="#" class="simple-text logo-normal">{{ _('White Dashboard') }}</a>
+        </div>
+        <ul class="nav">
+            <li @if ($pageSlug == 'dashboard') class="active " @endif>
+                <a href="{{ route('home') }}">
+                    <i class="tim-icons icon-chart-pie-36"></i>
+                    <p>{{ _('Dashboard') }}</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
